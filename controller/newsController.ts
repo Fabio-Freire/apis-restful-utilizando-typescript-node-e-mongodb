06485d8c2bcdd01bdfee59171e26290b0bc6c5e7
Fabio-Freire <fabiofreire84@gmail.com>
2022-08-03 10:34:05 -0300

    Criado  service e controller

---
+++ controller/newsController.ts
@@ -0,0 +1,16 @@
+import newsService from "../services/newsService";
+
+class NewsController{
+get(req, res){}
+
+getByid(req, res){}
+
+create(req, res){}
+
+update(req, res){}
+
+delete(req, res){}
+
+}
+
+export default new NewsController();
