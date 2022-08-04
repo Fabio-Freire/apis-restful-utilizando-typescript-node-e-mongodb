e997100848fd270459efef90c39646da60deaddc
Fabio-Freire <fabiofreire84@gmail.com>
2022-08-04 06:43:00 -0300

    Metodos do controller criados

--- controller/newsController.ts
+++ controller/newsController.ts
@@ -1,15 +1,55 @@
 import newsService from "../services/newsService";
+import * as HttpStatus from "http-status"
 
-class NewsController{
-get(req, res){}
+class NewsController {
 
-getByid(req, res){}
+    sendResponse = function (res, statusCode, data) {
+        res.status(statusCode).json(data);
+    }
 
-create(req, res){}
+    get(req, res) {
+        newsService.get()
+            .then(news => this.sendResponse(res, HttpStatus.Ok, news))
+            .catch(error => console.error.bind(console, 'Error' + error));
+    }
 
-update(req, res){}
+    getByid(req, res) {
+        const _id = req.params.id;
 
-delete(req, res){}
+        newsService.getById(_id)
+            .then(news => this.sendResponse(res, HttpStatus.Ok, news))
+            .catch(error => console.error.bind(console, 'Error' + error));
+
+    }
+
+    create(req, res) {
+        let vm = req.body;
+
+        newsService.create(vm)
+            .then(news =>
+                this.sendResponse(res, HttpStatus.OK, "Noticia cadastrada com sucesso!"))
+            .catch(error => console.error.bind(console, 'Error' + error));
+    }
+
+    update(req, res) {
+        const _id = req.params.id;
+        let news = req.body;
+
+        newsService.create(news)
+            .then(news =>
+                this.sendResponse(res, HttpStatus.OK, news.title + " foi atualizada com sucesso!"))
+            .catch(error => console.error.bind(console, 'Error' + error));
+
+    }
+
+    delete(req, res) {
+        const _id = req.params.id;
+
+        newsService.delete(_id)
+            .then(() =>
+                this.sendResponse(res, HttpStatus.OK, "Noticia deletada com sucesso!"))
+            .catch(error => console.error.bind(console, 'Error' + error));
+    }
 
 }
 
